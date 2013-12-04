1617167237ef6712707e85a2417c90c6dc7c2b67
yangzhj6 <695300838@qq.com>
2013-12-04 11:25:39 +0800

    Initial Commit

---
+++ zJ_Poker_Demo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  zJ_Poker_Demo
+//
+//  Created by zJ on 13-12-4.
+//  Copyright (c) 2013年 zJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ zJ_Poker_Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  zJ_Poker_Demo
+//
+//  Created by zJ on 13-12-4.
+//  Copyright (c) 2013年 zJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
